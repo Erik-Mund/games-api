3f603a167810ed8254ae6d3f9901a04d2f48b9eb
Erik-Mund <271922164+Erik-Mund@users.noreply.github.com>
2026-05-12 15:31:39 +0300

    fixed DB connection issue

--- GameBaseAPI/app.py
+++ GameBaseAPI/app.py
@@ -50,9 +50,6 @@ def create_app(config_name=None):
         if db_url.startswith("postgres://"):
             db_url = db_url.replace("postgres://", "postgresql://", 1)
 
-        if "sslmode=" not in db_url:
-            db_url += "?sslmode=require"
-
         app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 
 
